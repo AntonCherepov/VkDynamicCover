167bd01c278f6bc6fa0f63e1c104df72300e1228
Anton Cherepov <anton.cherepov.97@gmail.com>
2019-09-08 06:29:33 +0300

    Added change cover when subscribing functionality

--- main.py
+++ main.py
@@ -20,8 +20,42 @@ def get_upload_url(api):
     return data['upload_url']
 
 
+def get_api():
+    session = vk.Session()
+    return vk.API(session, v=5.101)
+
+
+def create_cover(data):
+    """ Функция создания обложки с аватаркой пользователя """
+
+    cov = Cover(os.path.join(images_dir, 'obl.jpg'))
+    try:
+        photo = data['photo_200']
+    except KeyError:
+        photo = data['photo_100']
+    ava = Avatar(photo)
+    ava.crap_corners()
+    cov.add_text(f'{data["first_name"]} {data["last_name"]}',
+                 os.path.join(fonts_dir, 'comicsansms.ttf'),
+                 font_size=35,
+                 height=300)
+    cov.add_user_image(ava.image, int((cov.w - ava.w) / 2), 100)
+    cov.save()
+
+
+def upload_cover(api):
+    upload_url = get_upload_url(api)
+    cover_file = {'photo': open(os.path.join(images_dir, 'final.jpg'),
+                                'rb')}
+    r = requests.post(upload_url, files=cover_file)
+    r_json = r.json()
+    api.photos.saveOwnerCoverPhoto(access_token=token,
+                                   hash=r_json['hash'],
+                                   photo=r_json['photo'])
+
+
 def change_cover(user_url, api, current_user_id):
-    """ Функция смены обложки при сообщении пользователя"""
+    """ Функция смены обложки при сообщении пользователя """
 
     message = 'Произошла неизвестная ошибка! Обратитесь к администратору.'
     try:
@@ -38,27 +72,8 @@ def change_cover(user_url, api, current_user_id):
             data['deactivated']
             message = 'Данный пользователь был удален.'
         except KeyError:
-            cov = Cover(os.path.join(images_dir, 'obl.jpg'))
-            try:
-                photo = data['photo_200']
-            except KeyError:
-                photo = data['photo_100']
-            ava = Avatar(photo)
-            ava.crap_corners()
-            cov.add_text(f'{data["first_name"]} {data["last_name"]}',
-                         os.path.join(fonts_dir, 'comicsansms.ttf'),
-                         font_size=35,
-                         height=300)
-            cov.add_user_image(ava.image, int((cov.w - ava.w) / 2), 100)
-            cov.save()
-            upload_url = get_upload_url(api)
-            cover_file = {'photo': open(os.path.join(images_dir, 'final.jpg'),
-                                        'rb')}
-            r = requests.post(upload_url, files=cover_file)
-            r_json = r.json()
-            api.photos.saveOwnerCoverPhoto(access_token=token,
-                                           hash=r_json['hash'],
-                                           photo=r_json['photo'])
+            create_cover(data)
+            upload_cover(api)
             message = f'Пользователь {data["first_name"]} \
                       {data["last_name"]} установлен на обложку.'
     except vk.exceptions.VkAPIError as err:
@@ -82,17 +97,22 @@ def main():
     """ Функция ожидания и обработки запросов от vk"""
 
     data = request.get_json()
+    api = get_api()
 
     if data['type'] == 'confirmation':
         return confirmation_token
-
     elif data['type'] == 'message_new':
-        session = vk.Session()
-        api = vk.API(session, v=5.101)
         user_id = data['object']['from_id']
-
         if int(data['object']['date']) > time() - 10:
             change_cover(data['object']['text'], api, user_id)
+    elif data['type'] == 'group_join':
+        user_id = data['object']['user_id']
+        data = api.users.get(access_token=token,
+                             user_ids=str(user_id),
+                             fields='photo_100, photo_200')[0]
+        create_cover(data)
+        upload_cover(api)
+    return 'OK'
 
 
 if __name__ == '__main__':
